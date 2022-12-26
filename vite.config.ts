e20eec9d313509a298a0dd40899422899f11d6ab
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-12-25 16:11:13 -0800

    setup build and deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-      outDir: './gh-pages'
+      outDir: './dist'
     },
     plugins: [svelte()],
 })
