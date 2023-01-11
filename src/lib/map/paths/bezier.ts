aea4b914e97b5222fb1b5c4b5ae69ea4ccae9a8d
iseau395 <70857830+iseau395@users.noreply.github.com>
2023-01-11 08:06:05 -0800

    add bezier curves

---
+++ src/lib/map/paths/bezier.ts
@@ -0,0 +1,44 @@
+import { register_insert_option } from "../../context_menu/context_menu";
+import { inch_pixel_ratio } from "../field";
+import { object, on } from "../objects/object";
+import { Point } from "./point";
+
+@object
+export class BezierCurve {
+    points: [Point, Point, Point, Point]
+
+    constructor(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) {
+        this.points = [
+            new Point(x1, y1),
+            new Point(x2, y2),
+            new Point(x3, y3),
+            new Point(x4, y4)
+        ];
+    }
+
+    @on("postrender")
+    render(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.moveTo(this.points[0].x, this.points[0].y);
+        ctx.bezierCurveTo(this.points[1].x, this.points[1].y, this.points[2].x, this.points[2].y, this.points[3].x, this.points[3].y);
+
+        ctx.strokeStyle = "#DD0000";
+        ctx.lineWidth = .5 * inch_pixel_ratio;
+        ctx.lineCap = "butt";
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.points[0].x, this.points[0].y);
+        ctx.lineTo(this.points[1].x, this.points[1].y);
+        ctx.moveTo(this.points[2].x, this.points[2].y);
+        ctx.lineTo(this.points[3].x, this.points[3].y);
+
+        ctx.setLineDash([10]);
+        ctx.strokeStyle = "#DD0000";
+        ctx.lineWidth = .5 * inch_pixel_ratio;
+        ctx.lineCap = "butt";
+        ctx.stroke();
+        ctx.setLineDash([]);
+    }
+
+}
--- src/lib/map/paths/path.ts
+++ src/lib/map/paths/path.ts
@@ -2,23 +2,28 @@ import { inch_pixel_ratio } from "../field";
 import { object, on } from "../objects/object";
 import { Point } from "./point";
 import { register_insert_option } from "../../context_menu/context_menu";
+import { BezierCurve } from "./bezier";
 
 @object
 export class Path {
-    readonly points = [
-        new Point(0, 10),
-        new Point(10, 10),
-        new Point(20, 10),
-        new Point(30, 10),
-        new Point(40, 10),
-    ];
+    readonly path: (Point|BezierCurve)[] = [];
 
     constructor() {
         register_insert_option({
             name: "Point",
             on_select: (x, y) =>
-                this.points.push(new Point(x/inch_pixel_ratio, y/inch_pixel_ratio))
+                this.path.push(new Point(x/inch_pixel_ratio, y/inch_pixel_ratio))
         });
+
+        register_insert_option({
+            name: "Bézier Curve",
+            on_select: (x, y) => {
+                x /= inch_pixel_ratio;
+                y /= inch_pixel_ratio;
+
+                this.path.push(new BezierCurve(x, y, x, y-15, x+20, y-15, x+20, y));
+            }
+        })
     }
 
     @on("postrender")
@@ -28,10 +33,10 @@ export class Path {
         ctx.lineWidth = .5 * inch_pixel_ratio;
         ctx.lineCap = "butt";
 
-        for (let i = 0; i < this.points.length-1; i++) {
+        for (let i = 0; i < this.path.length-1; i++) {
             ctx.beginPath();
-            ctx.moveTo(this.points[i].x, this.points[i].y);
-            ctx.lineTo(this.points[i+1].x, this.points[i+1].y);
+            ctx.moveTo(this.path[i].x, this.path[i].y);
+            ctx.lineTo(this.path[i+1].x, this.path[i+1].y);
             ctx.stroke();
         }
     }
